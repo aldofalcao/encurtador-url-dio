03ca437eb10d674e6f97e475b673394cf0cfbe52
aldofalcao <clodoaldo.falcao@gmail.com>
2022-01-10 11:45:11 -0400

    Commit projeto nodejs para dio

---
+++ src/config/Constants.ts
@@ -0,0 +1,4 @@
+export const config = {
+  API_URL: "http://localhost:5000",
+  MONGO_CONNECTION: "mongodb+srv://aldo:141685@encurtadorurldio.fy4ub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+}
